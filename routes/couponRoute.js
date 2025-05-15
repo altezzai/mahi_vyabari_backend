12deff0072c6eb12a26b89f7541a03c2e4c07604
mishab <mishab1441@gmail.com>
2025-05-15 17:36:29 +0530

    updated coupon routes

--- routes/couponRoute.js
+++ routes/couponRoute.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-
 const coupenController = require("../controllers/couponController");
 
 router.post("/request-coupon", coupenController.requestCoupen);
@@ -10,13 +9,12 @@ router.put(
 );
 router.post("/assign-shop-coupon", coupenController.assignShopCoupon);
 router.post("/assign-user-coupon", coupenController.assignUserCoupon);
-
 router.get("/get-shops",coupenController.getShops);
 router.get("/get-users",coupenController.getUsers);
 router.get("/get-recent-user-coupons",coupenController.getRecentUserCoupons);
 router.get("/get-shop-current-coupon-status",coupenController.getCurrentShopCouponStatus);
 router.get("/get-user-coupon-status",coupenController.getUserCouponStatus)
-router.get("/get-pending-coupons",coupenController.getPendingCoupons)
+router.get("/get-pending-coupons",coupenController.getPendingCoupons);
 router.get("/get-coupon-requests", coupenController.getCouponRequests);
 router.get("/get-assigned-coupons", coupenController.getAssignedCoupon);
 router.get("/get-coupon-history", coupenController.getCouponHistory);
