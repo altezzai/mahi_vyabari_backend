8b852f7b6081e395626d51c0c66e8b5cf30dcca2
ishaque risan <riswanp747970@gmail.com>
2025-12-05 15:18:31 +0530

    change table name Places to places

--- migrations/20251205082708-create-place.js
+++ migrations/20251205082708-create-place.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("Places", {
+    await queryInterface.createTable("places", {
       id: {
         type: Sequelize.INTEGER,
         primaryKey: true,
@@ -29,6 +29,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("Places");
+    await queryInterface.dropTable("places");
   },
 };
